797def1d8e5b4ddc36e5bc20f48c553bd7b49334
SUN BO <starry@vip.qq.com>
2020-05-14 13:40:17 +0800

    修改生成路由对象

--- src/builders/router-builder.ts
+++ src/builders/router-builder.ts
@@ -13,18 +13,6 @@ export class RouterBuilder {
   }
 
   public build(): VueRouter {
-    let router = new VueRouter({
-      mode: 'history',
-      scrollBehavior: (to, from, savedPosition) => {
-        if (savedPosition) {
-          return savedPosition
-        } else {
-          return { x: 0, y: 0 }
-        }
-      },
-      base: process.env.BASE_URL,
-      routes: this.context.routes.constants
-    })
-    return router
+    return this.context.routes.create()
   }
 }
