5a3fcf8b9f54a1288b7818281e6b68745717e5c5
SUN BO <starry@vip.qq.com>
2020-05-14 14:09:13 +0800

    导出 store

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,20 @@
 import { Builder } from './builder'
 import { Context } from './context'
 import * as models from './models'
+import * as store from './store'
 import * as utils from './utils'
 export { models }
 export { utils }
 export { Builder }
 export { Context }
-export default { models, utils, Builder, Context }
+export { store }
+export default { models, utils, store, Builder, Context }
 declare global {
   interface Date {
     toSmartString(): string
   }
+}
+
+Date.prototype.toSmartString = function () {
+  return utils.times.utils.toSmartString(this)
 }
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,11 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-import { IAppState } from './modules/app'
-import { IErrorLogState } from './modules/error-log'
-import { IPermissionState } from './modules/permission'
-import { ISettingsState } from './modules/settings'
-import { ITagsViewState } from './modules/tags-view'
-import { IUserState } from './modules/user'
+import { AppModule, IAppState } from './modules/app'
+import { ErrorLogModule, IErrorLogState } from './modules/error-log'
+import { IPermissionState, PermissionModule } from './modules/permission'
+import { ISettingsState, SettingsModule } from './modules/settings'
+import { ITagsViewState, TagsViewModule } from './modules/tags-view'
+import { IUserState, UserModule } from './modules/user'
 
 Vue.use(Vuex)
 
@@ -18,5 +18,9 @@ export interface IRootState {
   settings: ISettingsState
 }
 
-// Declare empty store first, dynamically register all modules later.
-export default new Vuex.Store<IRootState>({})
+export { AppModule }
+export { ErrorLogModule }
+export { PermissionModule }
+export { SettingsModule }
+export { TagsViewModule }
+export { UserModule }
